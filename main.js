7366a72bfbd2a0240586bb40ed56c4dc8e51460d
Eben Palazzolo <olozzalap@gmail.com>
2015-11-11 01:11:11 -0500

    Milestone 8: Scope Scuba Completed

--- main.js
+++ main.js
@@ -28,5 +28,9 @@ var app = angular.module('travel-app', [
     }.run( function() {
       console.log("Angular app running!");
     }).controller('destController', ['$scope', '$stateParams', 'title', function($scope, $stateParams, title){
+      $scope.all_dests = [{id: 1, location: "Berlin", cost: "$472", img: "http://www.telegraph.co.uk/travel/destination/article128328.ece/ALTERNATES/w620/berlin.jpg"}, {id: 2, location: "Hong Kong", cost: "$1312", img: "http://cache-graphicslib.viator.com/graphicslib/thumbs674x446/3675/SITours/hong-kong-harbor-night-cruise-and-dinner-at-victoria-peak-in-hong-kong-114436.jpg"}, {id: 3, location: "London", cost: "$650", img: "http://cdn.londonandpartners.com/assets/73295-640x360-london-skyline-ns.jpg"}];
+      $scope.dest_id = $stateParams.id;
+
+      $scope.title = title.value;
     })
   )

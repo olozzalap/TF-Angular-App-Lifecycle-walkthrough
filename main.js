7abf55537b3f102a92521d3f5c85903d822f3780
Eben Palazzolo <olozzalap@gmail.com>
2015-11-11 01:05:29 -0500

    Milestone 4: Routing in Russia Completed

--- main.js
+++ main.js
@@ -2,6 +2,18 @@ var app = angular.module('travel-app', [
   'ui.router',
   ]).config( 
   	function($stateProvider, $urlRouterProvider) {
+      $urlRouterProvider.otherwise('/list');
 
+      $stateProvider
+        .state('list', {
+          url: '/list',
+          templateUrl: 'list.html',
+          controller: 'destController',
+        })
+        .state('destination', {
+          url: '/dest/:id',
+          templateUrl: 'dest.html',
+          controller: 'destController',
+        })
     }	
   )
